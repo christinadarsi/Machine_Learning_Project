db836a418066079ea9be770eb0d39b4e76fc7d71
Christina Darsi <christina.darsi@gmail.com>
2025-04-28 16:04:08 -0500

    setup & Requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setuptools
+
+
+setup(
+name='mlproject',
+version='0.0.1',
+author='Christina_Darsi',
+author_email='christina.darsi230013@gmail.com',
+packages=find_packages(),
+install_requires=['pandas','numpy','seaborn']
+)
+
