d9e0c1acf114c3cf05aabd30e97974449539c008
Christina Darsi <christina.darsi@gmail.com>
2025-05-02 13:52:44 -0500

    Data Transformation pipeline

--- src/exception.py
+++ src/exception.py
@@ -1,16 +1,17 @@
 import sys
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message="Error occured in Python Script name [{0}] line number [{1}] error message[{2}]".format()
-    file_name,exc_tb.tb_lineno,str(error)
-    return error_message
+
+def error_message_detail(error_message, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    return "Error occurred in script: [{0}], line: [{1}], message: [{2}]".format(
+        file_name, line_number, str(error_message)
+    )
 
 class CustomException(Exception):
-    def __init__(self, error_message,error_detail:sys):
-        super().__init__(error_message) 
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+    def __init__(self, error_message, error_detail: sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail)
 
     def __str__(self):
-        return self.error_message    
-    
+        return self.error_message
